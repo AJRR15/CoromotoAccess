b4867748d9c0d6e4b4221e3436c1043414949afb
Alejrr2 <124023428+Alejrr2@users.noreply.github.com>
2024-12-19 15:25:53 -0600

    Visuales

---
+++ CoromotoAccess/CoromotoAccess/Controllers/EmpleadosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoromotoAccess.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        // GET: Empleados
+        public ActionResult GestionEmpleados()
+        {
+            return View();
+        }
+        public ActionResult ModificarEmpleado()
+        {
+            return View();
+        }
+        public ActionResult EmpleadoTurno()
+        {
+            return View();
+        }
+        public ActionResult EmpleadosMain()
+        {
+            return View();
+        }
+        public ActionResult CrearTurno()
+        {
+            return View();
+        }
+        public ActionResult AsignarTurno()
+        {
+            return View();
+        }
+    }
+}
+    
---
+++ CoromotoAccess/CoromotoAccess/Controllers/RecursosHumanosController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoromotoAccess.Controllers
+{
+    public class RecursosHumanosController : Controller
+    {
+        // GET: RecursosHumanos
+        public ActionResult GestiondeRRHH()
+        {
+            return View();
+        }
+        public ActionResult GestionarVacaciones()
+        {
+            return View();
+        }
+        public ActionResult AutoGestion()
+        {
+            return View();
+        }
+        public ActionResult ConsultarBoletasPago()
+        {
+            return View();
+        }
+        public ActionResult AprobarVacaciones()
+        {
+            return View();
+        }
+        public ActionResult GestionNomina()
+        {
+            return View();
+        }
+        public ActionResult EvaluacionEmpleados()
+        {
+            return View();
+        }
+
+
+    }
+    
+}
