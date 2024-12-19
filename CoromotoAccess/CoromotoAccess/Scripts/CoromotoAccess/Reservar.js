ee520d94855bff6adfa8df955219e4a04b6c7d0d
Alejrr2 <124023428+Alejrr2@users.noreply.github.com>
2024-12-19 15:20:38 -0600

    Cambio visuales

--- CoromotoAccess/CoromotoAccess/Scripts/CoromotoAccess/Reservar.js
+++
@@ -1,9 +0,0 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    var reservarForm = document.getElementById('reservarForm');
-
-    reservarForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        alert('Reservación exitosa');
-        $('#reservarModal').modal('hide');
-    });
-});
--- CoromotoAccess/CoromotoAccess/Styles/FuncionesJS/Productos.js
+++
@@ -1,4 +0,0 @@
-﻿$(document).ready(function () {
-    $().UItoTop({ easingType: 'easeOutQuart' });
-
-});
