5ae51a53c20f105c6926cdd5c78a8dbf70f6a200
Alejrr2 <124023428+Alejrr2@users.noreply.github.com>
2024-12-19 01:02:52 -0600

    Sistema

---
+++ CoromotoAccess/CoromotoAccess/Controllers/EmpleadosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoromotoAccess.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        // GET: Empleados
+        public ActionResult GestionEmpleados()
+        {
+            return View();
+        }
+        public ActionResult ModificarEmpleado()
+        {
+            return View();
+        }
+        public ActionResult EmpleadoTurno()
+        {
+            return View();
+        }
+        public ActionResult EmpleadosMain()
+        {
+            return View();
+        }
+    }
+}
+    
