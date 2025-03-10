a9ac8db4cfe54a8c893fe972d0eee5ee8cca9262
jonafide <jesquivel40315@ufide.ac.cr>
2025-03-09 19:51:56 -0600

    Historia 9.4 actualización de comentarios de evaluaciones de empleados.

--- CoromotoAccess/CoromotoAccess/Controllers/RecursosHumanosController.cs
+++ CoromotoAccess/CoromotoAccess/Controllers/RecursosHumanosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoromotoAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -33,9 +34,70 @@ namespace CoromotoAccess.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult EvaluacionEmpleados()
         {
-            return View();
+            using (var context = new BDCoromotoEntities())
+            {
+                var empleados = context.tEmpleados.Select(e => new Empleado
+                {
+                    ConsecutivoEmp = e.ConsecutivoEmp,
+                    Nombre = e.Nombre,
+                    Apellido = e.Apellido,
+                    IdRol = e.ConsecutivoRol,
+                    HorasTrabajadas = e.HorasTrabajadas
+                }).ToList();
+
+                ViewBag.Comentarios = context.tEvaluaciones.ToDictionary(c => c.IdEmpleado, c => c.Comentario);
+                ViewBag.Calificaciones = context.tEvaluaciones.ToDictionary(c => c.IdEmpleado, c => c.Calificacion);
+                ViewBag.DiccionarioRoles = context.tRoles.ToDictionary(r => r.IdRol, r => r.NombreRol);
+                ViewBag.DiccionarioCalificaciones = new Dictionary<int, string> { { 0, "Sin calificar" }, { 1, "Pobre" }, { 2, "Regular" }, { 3, "Bueno" }, { 4, "Excelente" } };
+                ViewBag.OpcionesCalificacion = new SelectList(ViewBag.DiccionarioCalificaciones, "Key", "Value");
+
+                ViewBag.TargetEmpleado = -1;
+
+                return View(empleados);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AsignarEvaluacion(Evaluacion model)
+        {
+            using (var context = new BDCoromotoEntities())
+            {
+                try
+                {
+                    var evaluacion = context.tEvaluaciones.FirstOrDefault(e => e.IdEmpleado == model.IdEmpleado);
+
+                    if (evaluacion != null)
+                    {
+                        evaluacion.Calificacion = model.Calificacion;
+                        evaluacion.Comentario = model.Comentario;
+                        evaluacion.FechaEvaluacion = DateTime.Now;
+                    }
+                    else
+                    {
+                        var nuevaEvaluacion = new tEvaluaciones
+                        {
+                            IdEmpleado = model.IdEmpleado,
+                            Comentario = model.Comentario,
+                            Calificacion = model.Calificacion,
+                            FechaEvaluacion = DateTime.Now
+                        };
+
+                        context.tEvaluaciones.Add(nuevaEvaluacion);
+                    }
+
+                    context.SaveChanges();
+
+                    return RedirectToAction("EvaluacionEmpleados");
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.MensajePantalla = $"Error: {ex.Message}";
+                    return RedirectToAction("EvaluacionEmpleados");
+                }
+            }
         }
         public ActionResult BoletasPago()
         {
@@ -45,12 +107,7 @@ namespace CoromotoAccess.Controllers
         {
             return View();
         }
-        
-            
-            
-
+    }
 
 
     }
-    
-}
---
+++ CoromotoAccess/CoromotoAccess/Models/Evaluacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoromotoAccess.Models
+{
+    public class Evaluacion
+    {
+        public int IdEvaluacion { get; set; }
+        public int IdEmpleado { get; set; }
+        public string Comentario { get; set; }
+        public int Calificacion { get; set; }
+        public DateTime? FechaEvaluacion { get; set; }
+    }
+}
